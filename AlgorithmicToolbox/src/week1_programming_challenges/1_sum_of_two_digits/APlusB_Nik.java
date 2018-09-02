c8e706f49bb5d36c5da327c9801e77cd776b5107
Nikita Baranov <NikitaBaranov@gmail.com>
2018-09-02 11:41:38 -0400

    Init Git Ignore

---
+++ AlgorithmicToolbox/src/week1_programming_challenges/1_sum_of_two_digits/APlusB_Nik.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class APlusB_Nik {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println(a + b);
+    }
+}
---
+++ AlgorithmicToolbox/src/week1_programming_challenges/t2_maximum_pairwise_product/MaxPairwiseProduct_Nik.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class MaxPairwiseProduct_Nik {
+
+    static long getMaxPairwiseProduct(int[] numbers) {
+        long max1 = 0L;
+        long max2 = 0L;
+
+        for (int el : numbers){
+            if (el > max1){
+                max2 = max1;
+                max1 = el;
+            }
+        }
+        return max1 * max2;
+    }
+
+    public static void main(String[] args) {
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        System.out.println(getMaxPairwiseProduct(numbers));
+    }
+
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new
+                    InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+}
